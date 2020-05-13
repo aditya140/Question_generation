90ee8ccd09b4e20d8495a8ff5d4b7c2b5c38fff7
root <theaditya140@gmail.com>
2020-05-13 06:50:59 +0000

    lightning version

--- src/debug.py
+++ src/debug.py
@@ -18,15 +18,14 @@ class Seq2seq_pl(pl.LightningModule):
     def __init__(self,hparams):
         super(Seq2seq_pl,self).__init__()
         self.hparams=hparams
-        self.model=Seq2seq(**hparams)
+        self.model=Seq2seq(**vars(hparams))
 
 
     def forward(self,input,target):
         return self.model.forward(input,target)
 
     def prepare_data(self):
-        data = SimpleDataloader(**vars(self.hp))
-
+        self.data = SimpleDataloader(**vars(self.hparams))
 
     def train_dataloader(self):
         return self.data.get_train_dataloader()
@@ -38,22 +37,22 @@ class Seq2seq_pl(pl.LightningModule):
         x,y,x_l = val_batch
         src=x
         trg=y
-        output=self.forward(src,trg)
-        output_dim = output.shape[-1]
-        output = output[1:,:].contiguous().view(-1, output_dim)
-        trg = trg[:,1:].contiguous().view(-1)
-        loss = self.cross_entropy_loss(output,trg)
+        pred=self.forward(src,trg)
+        pred=pred.permute(1,2,0)
+        loss = self.cross_entropy_loss(pred,trg)
         return {'val_loss': loss}
+
+    def validation_epoch_end(self, outputs):
+        print(outputs)
+        return 
 
     def training_step(self,train_batch,idx):
         x,y,x_l= train_batch
         src=x
         trg=y
-        output=self.forward(src,trg,teacher_forcing=True)
-        output_dim = output.shape[-1]
-        output = output[1:,:].contiguous().view(-1, output_dim)
-        trg = trg[:,1:].contiguous().view(-1)
-        loss = self.cross_entropy_loss(output,trg)
+        pred=self.forward(src,trg)
+        pred=pred.permute(1,2,0)
+        loss = self.cross_entropy_loss(pred,trg)
         logs = {'train_loss': loss}
         tensorboard_logs = {'train_loss': loss}
         return {'loss': loss, 'log': logs}
@@ -67,19 +66,10 @@ class Seq2seq_pl(pl.LightningModule):
 
 
 def main():
-    model=Seq2seq_pl(hparams)
-    model.prepare_data()
-    # writer = SummaryWriter("tb_logs/mnist/")
-    test_inp=next(iter(model.val_dataloader()))
-    print(test_inp[0].shape,test_inp[1].shape)
-    model.eval()
-    print(model(test_inp[0],test_inp[1]))
-    traced_script_module = torch.jit.trace(model, (test_inp[0],test_inp[1]))
-    # writer.add_graph(model,(test_inp[0],test_inp[1]),verbose=True)
-    # writer.close()
-
-    trainer=pl.Trainer(max_epochs=2,gpus=1,logger=logger)
+    model=Seq2seq_pl(hp)
+    trainer=pl.Trainer(max_epochs=hp.epochs,gpus=1,logger=logger,auto_lr_find=True)
     trainer.fit(model)
+    save_model(model,hp.save_path)
 
 if __name__ == '__main__':
     main()
