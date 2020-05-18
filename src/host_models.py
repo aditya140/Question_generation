7dbef94e6c71c27aefbd93dd76ca2ae4ba7a11ea
root <theaditya140@gmail.com>
2020-05-18 08:50:48 +0000

    streamlit

--- src/host_models.py
+++ src/host_models.py
@@ -20,7 +20,7 @@ def load_st_model(name,version):
         trg_pad_idx=optLang.word2idx[optLang.special["pad_token"]]
         model = transformer(src_pad_idx=src_pad_idx,trg_pad_idx=trg_pad_idx,**hp)
     model.load_state_dict(state_dict)
-    return model,inpLang,optLang
+    return model,inpLang,optLang,hp
 
 
 st.sidebar.title("Question Generation Models")
@@ -28,7 +28,13 @@ st.sidebar.title("Question Generation Models")
 model_name=st.sidebar.selectbox("Model",["seq2seq","transformer"])
 version = get_max_version(model_name)
 version_selected = st.sidebar.slider('Select version',min_value=min(version),max_value=max(version))
-model,inpLang,optLang=load_st_model(model_name,version_selected)
+model,inpLang,optLang,hp=load_st_model(model_name,version_selected)
+
+if hp["squad"]==False:
+    st.markdown("## Translation Model")
+else:
+    st.markdown("## Question Generation Model")
+
 
 dec_type = st.radio( "Decoding Type",('Greedy', 'Beam'))
 max_length=st.slider("Max Decoding Length",2,100)
@@ -50,7 +56,7 @@ txt = st.text_area('Context', "john used to live in canada")
 st.markdown("# Output")
 
 if dec_type=="Greedy":
-    st.text(" ".join(decoder.greedy(txt,max_len=max_length)))
+    st.markdown(" ".join(decoder.greedy(txt,max_len=max_length)))
 elif dec_type=="Beam":
     outputs = decoder.beam("where dows the ",max_len=max_length,beam_width=beam_size)
     for idx,i in enumerate(outputs):
