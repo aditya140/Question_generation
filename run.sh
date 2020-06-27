a928a9661d9763ddc02125c2f9ace0aab3d98f3d
Aditya Parkhi <theaditya140@gmail.com>
2020-06-26 21:33:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 nvidia-smi
 python3 test_cuda.py
-python3 src/trainProcs/transformer/transformer.py --gpu --to_artifact --epochs 1 | tee /artifacts/test_log.log
+python3 src/trainProcs/transformer/transformer.py --gpu --to_artifact --epochs 1 --sample| tee /artifacts/test_log.log
+python3 src/trainProcs/seq2seq/seq2seq.py --gpu --to_artifact --epochs 1 --sample | tee /artifacts/test_log.log
